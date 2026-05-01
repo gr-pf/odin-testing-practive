f3a09c90042720ec4a9481f3f95ba6723d9e4940
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-05-01 15:34:35 +0200

    Update config files

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,18 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import jest from "eslint-plugin-jest";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ["**/*.test.js"],
+    plugins: { jest },
+    env: { jest: true },
+  },
 ]);
